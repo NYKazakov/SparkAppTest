9bcf30593b39234c6b08d228f3ffe84e5f6d468e
Казаков Николай Юрьевич <Kazakov.N.Y@sberbank.ru>
2021-07-19 06:07:57 +0300

    Initial

---
+++ build.sbt
@@ -0,0 +1,41 @@
+ThisBuild / name := "BeelineTest"
+
+ThisBuild / version := "0.1"
+
+ThisBuild / scalaVersion := "2.11.12"
+
+val sparkVersion = "2.4.0"
+
+//lazy val beelinetest = (project in file("."))
+//  .settings(
+//    assembly / assemblyJarName := "BeelineTestSolution.jar",
+//    assembly / mainClass := Some("ru.noname.Solution")
+//  )
+
+ThisBuild / libraryDependencies ++= Seq(
+  "org.apache.spark" %% "spark-core" % sparkVersion,
+  "org.apache.spark" %% "spark-sql" % sparkVersion
+)
+
+ThisBuild / assemblyMergeStrategy := {
+  case PathList("org","aopalliance", xs @ _*) => MergeStrategy.last
+  case PathList("javax", "inject", xs @ _*) => MergeStrategy.last
+  case PathList("javax", "servlet", xs @ _*) => MergeStrategy.last
+  case PathList("javax", "activation", xs @ _*) => MergeStrategy.last
+  case PathList("org", "apache", xs @ _*) => MergeStrategy.last
+  case PathList("com", "google", xs @ _*) => MergeStrategy.last
+  case PathList("com", "esotericsoftware", xs @ _*) => MergeStrategy.last
+  case PathList("com", "codahale", xs @ _*) => MergeStrategy.last
+  case PathList("com", "yammer", xs @ _*) => MergeStrategy.last
+  case "about.html" => MergeStrategy.rename
+  case "META-INF/ECLIPSEF.RSA" => MergeStrategy.last
+  case "META-INF/mailcap" => MergeStrategy.last
+  case "META-INF/mimetypes.default" => MergeStrategy.last
+  case "plugin.properties" => MergeStrategy.last
+  case "log4j.properties" => MergeStrategy.last
+  case "git.properties" => MergeStrategy.last
+  case "overview.html" => MergeStrategy.last  // Added this for 2.1.0 I think
+  case x =>
+    val oldStrategy = (assemblyMergeStrategy in assembly).value
+    oldStrategy(x)
+}
---
+++ src/main/scala/ru/noname/Solution.scala
@@ -0,0 +1,73 @@
+package ru.noname
+
+import org.apache.spark.sql.functions.{max, sum}
+import org.apache.spark.sql.types._
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+object Solution {
+  def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession.builder
+      .appName("Solution")
+      .getOrCreate()
+
+    val customerSchema = new StructType()
+      .add("id", IntegerType, nullable = false)
+      .add("name", StringType, nullable = true)
+      .add("email", StringType, nullable = true)
+      .add("joinDate", DateType, nullable = true)
+      .add("status", StringType, nullable = true)
+    val customerDF: DataFrame = spark.read
+      .options(Map("delimiter" -> "\t"))
+      .format("csv")
+      .schema(customerSchema)
+      .load("sol/customer.csv")
+    customerDF.createTempView("customers")
+
+    val productSchema = new StructType()
+      .add("id", IntegerType, nullable = false)
+      .add("name", StringType, nullable = true)
+      .add("price", DoubleType, nullable = true)
+      .add("numberOfProducts", IntegerType, nullable = true)
+    val productDF: DataFrame = spark.read
+      .options(Map("delimiter" -> "\t"))
+      .format("csv")
+      .schema(productSchema)
+      .load("sol/product.csv")
+    productDF.createTempView("products")
+
+    val orderSchema = new StructType()
+      .add("customerID", IntegerType, nullable = false)
+      .add("orderID", IntegerType, nullable = true)
+      .add("productID", IntegerType, nullable = true)
+      .add("numberOfProduct", IntegerType, nullable = true)
+      .add("orderDate", DateType, nullable = true)
+      .add("status", StringType, nullable = true)
+    val orderDF: DataFrame = spark.read
+      .options(Map("delimiter" -> "\t"))
+      .format("csv")
+      .schema(orderSchema)
+      .load("sol/order.csv")
+
+    val sumProductsByIDs = orderDF
+      .filter("status = 'delivered'")
+      .groupBy("customerID", "productID")
+      .agg(sum("numberOfProduct") as "sumProduct")
+    sumProductsByIDs.createTempView("orders")
+
+    val maxOrderedByCustomer = sumProductsByIDs
+      .groupBy("customerID")
+      .agg(max("sumProduct") as "maximum")
+    maxOrderedByCustomer.createTempView("max_orders")
+
+    val result = spark.sql(
+      """SELECT c.name as customer_name, p.name as product_name
+        |FROM orders o
+        |INNER JOIN max_orders m ON o.customerID = m.customerID
+        |INNER JOIN products p ON o.productID = p.id
+        |INNER JOIN customers c ON o.customerID = c.id
+        |AND c.status = 'active'
+        |AND sum(o.numberOfProduct) = m.maximum""".stripMargin)
+    result.write.csv("result")
+  }
+}
